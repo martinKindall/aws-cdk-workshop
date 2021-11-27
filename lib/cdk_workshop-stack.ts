346e6cb5e3a9db6258b5bbd6f3f86bd45d969813
Martin <mart256@gmail.com>
2021-11-27 20:58:58 +0100

    Created an http api and added the lambda as integration

--- lib/cdk_workshop-stack.ts
+++ lib/cdk_workshop-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda'
+import * as apigw from '@aws-cdk/aws-apigatewayv2'
+import { LambdaProxyIntegration } from '@aws-cdk/aws-apigatewayv2-integrations'
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -9,6 +11,17 @@ export class CdkWorkshopStack extends cdk.Stack {
       runtime: lambda.Runtime.NODEJS_14_X,
       code: lambda.Code.fromAsset('lambda'),
       handler: 'hello.handler'
+    });
+
+    const lambdaIntegration = new LambdaProxyIntegration({
+      handler: hello
+    })
+
+    const httpApi = new apigw.HttpApi(this, 'MyHttpApi');
+    httpApi.addRoutes({
+      path: '/hello',
+      methods: [apigw.HttpMethod.GET],
+      integration: lambdaIntegration
     });
   }
 }

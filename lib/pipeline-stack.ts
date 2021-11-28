41eb27a2cf06754a2ff2750a7c42967d23ba0e16
Martin <mart256@gmail.com>
2021-11-28 23:00:51 +0100

    Using github instead of codecommit as source. Commented app code, just because it is not used.

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,5 +1,4 @@
 import * as cdk from '@aws-cdk/core'
-import * as codecommit from '@aws-cdk/aws-codecommit'
 import {CodeBuildStep, CodePipeline, CodePipelineSource} from "@aws-cdk/pipelines";
 import {MyPipelineStage} from "./pipeline-stage";
 
@@ -7,14 +6,18 @@ export class WorkshopPipelineStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const repo = new codecommit.Repository(this, 'WorkshopRepo', {
-            repositoryName: 'WorkshopRepo'
-        });
+        const githubOwnerRepo = 'martinKindall/aws-cdk-workshop';
+        const bodeBuildSource = CodePipelineSource.connection(
+            githubOwnerRepo,
+            'main',
+            {
+                connectionArn: 'arn:aws:codestar-connections:us-east-1:371417955885:connection/7a804859-2d9e-43c0-b2ea-47ca243c60d9',
+        })
 
         const pipeline = new CodePipeline(this, 'Pipeline', {
             pipelineName: 'MyCDKPipeline',
             synth: new CodeBuildStep('SynthStep', {
-                input: CodePipelineSource.codeCommit(repo, 'main'),
+                input: bodeBuildSource,
                 installCommands: [
                     'npm install -g aws-cdk'
                 ],
@@ -26,29 +29,29 @@ export class WorkshopPipelineStack extends cdk.Stack {
             })
         });
 
-        const workShopStage = new MyPipelineStage(this, 'DeployStage');
-        const deployStage = pipeline.addStage(workShopStage);
-        deployStage.addPost(
-            new CodeBuildStep('TestViewerEndpoint', {
-                projectName: 'TestViewerEndpoint',
-                envFromCfnOutputs: {
-                    ENDPOINT_URL: workShopStage.myTableViewerUrl
-                },
-                commands: [
-                    'curl -Ssf $ENDPOINT_URL'
-                ]
-            }),
-
-            new CodeBuildStep('TestApiGatewayEndpoint', {
-                projectName: 'TestApiGatewayEndpoint',
-                envFromCfnOutputs: {
-                    ENDPOINT_URL: workShopStage.httpApiUrl
-                },
-                commands: [
-                    'curl -Ssf $ENDPOINT_URL/hello',
-                    'curl -Ssf $ENDPOINT_URL/dogs'
-                ]
-            })
-        );
+        // const workShopStage = new MyPipelineStage(this, 'DeployStage');
+        // const deployStage = pipeline.addStage(workShopStage);
+        // deployStage.addPost(
+        //     new CodeBuildStep('TestViewerEndpoint', {
+        //         projectName: 'TestViewerEndpoint',
+        //         envFromCfnOutputs: {
+        //             ENDPOINT_URL: workShopStage.myTableViewerUrl
+        //         },
+        //         commands: [
+        //             'curl -Ssf $ENDPOINT_URL'
+        //         ]
+        //     }),
+        //
+        //     new CodeBuildStep('TestApiGatewayEndpoint', {
+        //         projectName: 'TestApiGatewayEndpoint',
+        //         envFromCfnOutputs: {
+        //             ENDPOINT_URL: workShopStage.httpApiUrl
+        //         },
+        //         commands: [
+        //             'curl -Ssf $ENDPOINT_URL/hello',
+        //             'curl -Ssf $ENDPOINT_URL/dogs'
+        //         ]
+        //     })
+        // );
     }
 }

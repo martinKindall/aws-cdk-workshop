a4379f5c97956eceed825cb86bf5fc480c243226
Martin <mart256@gmail.com>
2021-11-28 18:59:38 +0100

    aws-cdk core and lambda match same version. Created first unit test.

---
+++ test/hitcounter.test.ts
@@ -0,0 +1,19 @@
+import { Template } from '@aws-cdk/assertions';
+import * as cdk from '@aws-cdk/core'
+import * as lambda from '@aws-cdk/aws-lambda'
+import {HitCounter} from "../lib/hitcounter";
+
+test('DynamoDB Table Created', () => {
+    const stack = new cdk.Stack();
+
+    new HitCounter(stack, 'MyTestConstruct', {
+        downstream:  new lambda.Function(stack, 'TestFunction', {
+            runtime: lambda.Runtime.NODEJS_14_X,
+            handler: 'hello.handler',
+            code: lambda.Code.fromAsset('lambda')
+        })
+    });
+
+    const template = Template.fromStack(stack);
+    template.resourceCountIs("AWS::DynamoDB::Table", 1);
+});

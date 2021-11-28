b046f65fbddb3fe09f79c1f648d3126a423487d2
Martin <mart256@gmail.com>
2021-11-28 19:20:21 +0100

    Created a unit test for the hitcounter lambda

--- test/hitcounter.test.ts
+++ test/hitcounter.test.ts
@@ -1,4 +1,4 @@
-import { Template } from '@aws-cdk/assertions';
+import {Capture, Template} from '@aws-cdk/assertions';
 import * as cdk from '@aws-cdk/core'
 import * as lambda from '@aws-cdk/aws-lambda'
 import {HitCounter} from "../lib/hitcounter";
@@ -16,4 +16,36 @@ test('DynamoDB Table Created', () => {
 
     const template = Template.fromStack(stack);
     template.resourceCountIs("AWS::DynamoDB::Table", 1);
+});
+
+test('Lambda exists and has env vars', () => {
+    const stack = new cdk.Stack();
+
+    new HitCounter(stack, 'MyTestConstruct', {
+        downstream: new lambda.Function(stack, 'MyLambda', {
+            runtime: lambda.Runtime.NODEJS_14_X,
+            code: lambda.Code.fromAsset('lambda'),
+            handler: 'hello.handler'
+        })
+    });
+
+    const template = Template.fromStack(stack);
+    const envCapture = new Capture();
+    template.resourceCountIs("AWS::Lambda::Function", 3);
+    template.hasResourceProperties("AWS::Lambda::Function", {
+        Environment: envCapture
+    });
+
+    expect(envCapture.asObject()).toEqual(
+        {
+            Variables: {
+                DOWNSTREAM_FUNCTION_NAME: {
+                    Ref: "MyLambdaCCE802FB"
+                },
+                HITS_TABLE_NAME: {
+                    Ref: "MyTestConstructHits24A357F0"
+                }
+            }
+        }
+    );
 });
